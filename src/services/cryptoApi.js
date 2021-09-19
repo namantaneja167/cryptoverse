1bc5efca89510e5648907346e2a6fd7e357703bf
Naman Taneja <42579074+namantaneja167@users.noreply.github.com>
2021-09-19 19:54:08 +0530

    GlobalStats and Redux

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi,fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders ={
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '939f2d7ecfmshe634b236a13d9eap192934jsnb88dac0be9e9'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url)=>({url,headers:cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath:'cryptoApi',
+    baseQuery:fetchBaseQuery({baseUrl}),
+    endpoints:(builder)=>({
+        getCryptos:builder.query({
+            query:()=>createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi
